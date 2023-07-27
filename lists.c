e7f71b07fe2efe02f3c9035daa58f2060a5d20be
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-07-26 17:34:10 -0700

    m

---
+++ lists.c
@@ -0,0 +1,121 @@
+#include "shell.h"
+
+/**
+ * list_len - determines len of linked list
+ * @h: ptr of 1st node
+ * Return: size
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * list_to_strings - fun to return an array of the linkd list
+ * @head: 1st nodes
+ * Return: array
+ */
+
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
+/**
+ * print_list - fun to print all elements of linked list
+ * @h: ptr to 1st nodes
+ * Return: size
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * node_starts_with - fun that returns prefixed nodes
+ * @node: ptr 1st nodes
+ * @prefix: match strig
+ * @c: char after prefix to match
+ * Return: match node or null
+ */
+
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * get_node_index - fun to get the index of a node
+ * @node: ptr
+ * @head: top nodes
+ * Return: indx node or -1
+ */
+
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
+}
