1193a5e6cdfdc019f78e82c5376f67ea679eb166
Mpethwane3110 <mpethwanenomfundo@gmail.com>
2023-07-28 02:05:47 -0700

    simple shell

--- getline.c
+++ getline.c
@@ -2,6 +2,114 @@
 #include <stddef.h>
 
 /**
+ * input_buf - fun to insert buffers in chaiined cmd
+ * @info: param structure with args
+ * @len: memory address of the length variable
+ * @buf: pointer of the buf
+ * Return: no. of bytes available
+ */
+
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0';
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
+
+/**
+ * get_input - fu to get a (line-new line)
+ * @info:param structure with args
+ * Return: amount of read bytes
+ */
+
+ssize_t get_input(info_t *info)
+{
+        static char *buf; /* the ';' command chain buffer */
+        static size_t i, j, len;
+        ssize_t r = 0;
+        char **buf_p = &(info->arg), *p;
+
+        _putchar(BUF_FLUSH);
+        r = input_buf(info, &buf, &len);
+        if (r == -1) /* EOF */
+                return (-1);
+        if (len)
+                {
+                j = i;
+                p = buf + i;
+                check_chain(info, buf, &j, i, len);
+                while (j < len)
+                        {
+                        if (is_chain(info, buf, &j))
+                                break;
+                        j++;
+                }
+
+                i = j + 1;
+                if (i >= len)
+                        {
+                        i = len = 0;
+                        info->cmd_buf_type = CMD_NORM;
+                }
+
+                *buf_p = p;
+                return (_strlen(p));
+                }
+
+        *buf_p = buf;
+        return (r);
+}
+
+/**
+ * read_buf - function to read a buffer
+ * @info: parameter structure
+ * @buf: the buffer
+ * @i: the size
+ * Return: (r)
+ */
+
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
+/**
  * _getline - gets input from STDIN
  * @info: prmtr struct
  * @ptr: memory address of pointer to buffer, preallocated or NULL
@@ -30,7 +138,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	c = _strchr(buf + i, '\n');
 	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
-	if (!new_p)
+	if (!new_p) /* MALLOC FAILURE! */
 		return (p ? free(p), -1 : -1);
 
 	if (s)
@@ -46,26 +154,6 @@ int _getline(info_t *info, char **ptr, size_t *length)
 		*length = s;
 	*ptr = p;
 	return (s);
-}
-
-/**
- * read_buf - function to read a buffer
- * @info: parameter structure
- * @buf: the buffer
- * @i: the size
- *
- * Return: (r)
- */
-ssize_t read_buf(info_t *info, char *buf, size_t *i)
-{
-	ssize_t r = 0;
-
-	if (*i)
-		return (0);
-	r = read(info->readfd, buf, READ_BUF_SIZE);
-	if (r >= 0)
-		*i = r;
-	return (r);
 }
 
 
